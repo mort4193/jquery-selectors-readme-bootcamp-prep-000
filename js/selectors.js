d23ec016b4260ae0b0bffe02424226ccba96335d
jonathan Morton <jmorton4193@gmail.com>
2017-10-22 00:59:52 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,6 @@
-// declare your functions here...
+function paragraphSelector (){$('p')}
+function lastImageSelector (){}
+function nijaBabySelector (){}
+function divSelector(){}
+function firstListItemSelector (){}
+
